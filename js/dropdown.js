590ff45b1d26a950da7aafa55aa7388144880bc3
Yura Shubin <166517574+Ashtray001@users.noreply.github.com>
2024-07-15 10:42:04 +0000

    upd

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,17 +1,21 @@
-const dropdowns = document.querySelectorAll('.dropdown');
-  
-  dropdowns.forEach(function(dropdown) {
-    let btn = dropdown.querySelector('.dropbtn');
-    let content = dropdown.querySelector('.dropdown-content');
+function setupDropdown(dropdownElement) {
+  let btn = dropdownElement.querySelector('.dropbtn');
+  let content = dropdownElement.querySelector('.dropdown-content');
 
-    btn.addEventListener('click', function() {
-      content.classList.toggle('show');
-    });
-
-    // Закриття випадаючого меню при кліку за межами
-    window.addEventListener('click', function(event) {
-      if (!dropdown.contains(event.target)) {
-        content.classList.remove('show');
-      }
-    });
+  btn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    content.classList.toggle('show');
   });
+
+  window.addEventListener('click', function(event) {
+    if (!dropdownElement.contains(event.target)) {
+      content.classList.remove('show');
+    }
+  });
+}
+
+// Викликаємо функцію setupDropdown для кожного елемента .dropdown, які ви хочете обробити
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(function(dropdown) {
+  setupDropdown(dropdown);
+});
